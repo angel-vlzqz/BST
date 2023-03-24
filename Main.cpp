f2db36ed6d232759b607d70f015c3333468d2975
angel velazquez <21velazquezang@gmail.com>
2023-03-24 13:39:02 -0700

    testing dunctions in main

--- Main.cpp
+++ Main.cpp
@@ -5,5 +5,19 @@ using namespace std;
 
 int main()
 {
-    cout << "test" << endl;
+    struct node *root = NULL;
+    root = insert(root, 70);
+    root = insert(root, 20);
+    root = insert(root, 10);
+    root = insert(root, 50);
+    root = insert(root, 60);
+    root = insert(root, 40);
+    root = insert(root, 5);
+    display(root);
+    cout << endl
+         << "is the data there? ";
+    cout << search(root, 10);
+    deleteNode(root, 40);
+    cout << endl;
+    display(root);
 }

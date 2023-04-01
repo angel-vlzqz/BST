1944ea3a696e093ceee28c40f32cc1ad065ccb01
angel velazquez <21velazquezang@gmail.com>
2023-03-31 17:51:54 -0700

    implimented UI

--- Main.cpp
+++ Main.cpp
@@ -22,21 +22,24 @@ int main()
     myFile.close();
     display(root);
     cout << endl;
-
-    // root = insert(root, 70);
-    // root = insert(root, 20);
-    // root = insert(root, 10);
-    // root = insert(root, 50);
-    // root = insert(root, 60);
-    // root = insert(root, 40);
-    // root = insert(root, 5);
-    // display(root);
-    // cout << endl
-    //      << "is the data there? ";
-    // cout << search(root, 10);
-    // deleteNode(root, 40);
-    // cout << endl;
-    // display(root);
-    // cout << endl;
     cout << "height of tree: " << getHeight(root) << endl;
+    cout << endl << endl << endl;
+
+    string loop = "test";
+    while (loop != "-1")
+    {
+        cout << "Enter a word you would like to look for. Enter -1 to quit: ";
+        cin >> loop;
+        cout << endl;
+
+        if (search(root, loop) && loop != "-1")
+        {
+            cout << loop << " was found in the tree!" << endl;
+        }
+        else
+        {
+            cout << loop << " was not found in the tree :(" << endl;
+        }
+    }
+
 }
